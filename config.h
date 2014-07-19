0aa7e5119714c33d916f3b0f58ffb45c550059e4
DuarteAfonso <duarteafonso@gmail.com>
2014-07-19 03:52:22 +0100

    codigo voo 19-07-2014
    -sem callsign
    -sem temperatura

--- config.h
+++ config.h
@@ -161,12 +161,12 @@
 
 //if any of the debugs is enabled, set soft serial parameters
 
- #define DEBUG_GPS    // GPS sentence dump and checksum validation
+// #define DEBUG_GPS    // GPS sentence dump and checksum validation
 #ifdef DEBUG_GPS  
   #define SOFTSERIALDEBUG  //allow the use of software serial TX on port A3
 #endif
 
- #define DEBUG_AX25   // AX.25 frame dump
+// #define DEBUG_AX25   // AX.25 frame dump
 #ifdef DEBUG_AX25  
   #define SOFTSERIALDEBUG  //allow the use of software serial TX on port A3
 #endif
@@ -186,7 +186,7 @@
   #define SOFTSERIALDEBUG  //allow the use of software serial TX on port A3
 #endif
 
-#define DEBUG_SENS   // Sensors
+// #define DEBUG_SENS   // Sensors
 #ifdef DEBUG_SENS  
   #define SOFTSERIALDEBUG  //allow the use of software serial TX on port A3
 #endif
